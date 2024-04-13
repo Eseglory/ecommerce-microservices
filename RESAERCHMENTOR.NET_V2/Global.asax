e2549e70c37b06d6471d2bf40b6d0b5b09b07091
Glory Eseosa <eseglory@hotmail.com>
2024-04-13 15:20:12 +0100

    rebuild

--- RESAERCHMENTOR.NET_V2/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RESAERCHMENTOR.NET_V2.MvcApplication" Language="C#" %>
--- delte Research.sql
+++
@@ -1,9 +0,0 @@
- 
-delete  [dbo].[AspNetRoles]
- delete [dbo].[AspNetUserClaims]
- delete [dbo].[AspNetUserLogins]
- delete [dbo].[AspNetUserRoles]
- delete [dbo].[AspNetUsers]
-delete  [dbo].[Following]
-delete  [dbo].[Research]
-delete [dbo].[Profile]
